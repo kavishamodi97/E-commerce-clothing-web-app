847de8bddeb73061ed0023cf53ed67db7c6c1688
kavishamodi97 <kavishamodi971@gmail.com>
2021-03-10 16:15:30 +0530

    custom input component and custom button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {Switch,Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Homepage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
 const HatsPage=()=>(
   <div>
     <h1>HATS PAGE</h1>
@@ -17,8 +20,10 @@ function App() {
     <Switch>
      <Route exact path='/' component={Homepage}/>
       <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
 }
+
 export default App;
