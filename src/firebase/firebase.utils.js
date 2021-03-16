d9cb7df95468e8c43ad0417392f532f7b039dbd2
kavishamodi97 <kavishamodi971@gmail.com>
2021-03-16 09:51:19 +0530

    implement fetch data from firestore into shop componet and added spinner functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -38,13 +38,42 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
     return userRef;
 };
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    console.log(collectionRef);
 
-export const auth=firebase.auth();
-export const firestore=firebase.firestore();
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+    return await batch.commit();
+}
 
-const provider=new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt:'select_account'});
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+        const { title, items } = doc.data();
 
-export const signInWithGoogle=()=>auth.signInWithPopup(provider);
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items,
+        };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
