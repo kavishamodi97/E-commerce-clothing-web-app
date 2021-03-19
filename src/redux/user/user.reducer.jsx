ea1074a2907a5caac3e9316eb326ff392018b57b
kavishamodi97 <kavishamodi971@gmail.com>
2021-03-19 16:13:14 +0530

    implemented redux saga in app

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,17 +1,33 @@
-import { UserActionTypes } from './user.types';
-const INITIAL_STATE={
-    currentUser:null
+import UserActionTypes from './user.types';
+
+const INITIAL_STATE = {
+    currentUser: null,
+    error: null
 };
 
-const userReducer=(state=INITIAL_STATE,action) => {
-    switch(action.type){
-        case UserActionTypes.SET_CURRENT_USER:
+const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
-                currentUser:action.payload
+                currentUser: action.payload,
+                error: null
             };
-            default:
-                return state;
+        case UserActionTypes.SIGN_OUT_SUCCESS:
+            return {
+                ...state,
+                currentUser: null,
+                error: null
+            };
+        case UserActionTypes.SIGN_IN_FAILURE:
+        case UserActionTypes.SIGN_OUT_FAILURE:
+        case UserActionTypes.SIGN_UP_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            };
+        default:
+            return state;
     }
 };
 
